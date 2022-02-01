6b3f07c97022c1f44824525f1f02f62cab1eff1e
Emiliaa1 <75319625+Emiliaa1@users.noreply.github.com>
2022-02-02 00:13:58 +0200

    Update newData.py

--- newData.py
+++ newData.py
@@ -1,3 +1,19 @@
 def verif_new_data(t,h,p):
-    for x in centroids:
+    n_clusters = centroids.shape[0]
+    n_samples = data.shape[0];
+    assign = {}
+    clusters = {}
+    for i in range(n_samples):
+    min_indx = -1
+    min_val = 1000000
+        
+    for j in range(n_clusters):
+        d = compute_distance(data[i],centroids[j])
+        if d<min_val:
+            min_val = d
+            min_indx = j
+    assign[i] = min_indx
+    clusters[min_indx].append(data[i])
+    return assign, clusters
+        
       
