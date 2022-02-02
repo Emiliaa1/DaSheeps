dba87bff662b40e113144b841b33f129a693f647
Emiliaa1 <75319625+Emiliaa1@users.noreply.github.com>
2022-02-02 06:10:08 -0800

    Update newData.py

--- newData.py
+++ newData.py
@@ -1,19 +1,17 @@
 def verif_new_data(t,h,p):
     n_clusters = centroids.shape[0]
-    n_samples = data.shape[0];
-    assign = {}
-    clusters = {}
-    for i in range(n_samples):
+    #create our new data point
+    new_data = [t,h,p]
+    
     min_indx = -1
     min_val = 1000000
-        
+    #Search for the closest centroid
     for j in range(n_clusters):
-        d = compute_distance(data[i],centroids[j])
+        d = compute_distance(new_data,centroids[j])
         if d<min_val:
             min_val = d
             min_indx = j
-    assign[i] = min_indx
-    clusters[min_indx].append(data[i])
-    return assign, clusters
+            
+    return min_indx
         
       
