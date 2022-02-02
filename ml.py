5cdcde656976a09d5c5a4c599b2676cb6e3956bc
Emiliaa1 <75319625+Emiliaa1@users.noreply.github.com>
2022-02-02 06:11:38 -0800

    Update ml.py

--- ml.py
+++ ml.py
@@ -2,6 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math 
 
+def verif_new_data(t,h,p):
+    n_clusters = centroids.shape[0]
+    #create our new data point
+    new_data = [t,h,p]
+    
+    min_indx = -1
+    min_val = 1000000
+    #Search for the closest centroid
+    for j in range(n_clusters):
+        d = compute_distance(new_data,centroids[j])
+        if d<min_val:
+            min_val = d
+            min_indx = j
+            
+    return min_indx
+
 # function for the distance between 2 points
 def compute_distance(x,y):
     dist = 0.0
