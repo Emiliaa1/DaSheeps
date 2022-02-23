c73ff952078c2680c89278ede9fe454c9988988c
Emiliaa1 <75319625+Emiliaa1@users.noreply.github.com>
2022-02-23 15:38:40 +0200

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,6 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import math 
+import sklearn.datasets as dataset
+
+def generate_data(n_samples=20, n_feats=2):
+  """ Generates simulated data.
+  Args:
+      n_samples (int): observed students.
+      n_feats (int): number of features characterizing each student.
+      n_groups (int): number of groups (clusters).
+  Returns:
+      x (ndarray): the features (inputs).
+  """
+  # We fix the seed to our random number generator to ensure that
+  # we always get the same psuedo-random number sequence.
+  np.random.seed(121)
+
+  # Sample features for n_samples students, distributed 
+  # around n_groups centers.
+  x, _ = dataset.make_blobs(n_samples=n_samples-2, 
+                            n_features=n_feats, 
+                            centers=[[9,21],[9, 12],[20,15],[20,8]],
+                            cluster_std=[[2,1], [1,1], [2,1], [2,1]],
+                            center_box = (5, 25))
+  # add two extra students to better emphasize our findings
+  x = np.append(x, [[1, 5], [15, 1]], axis=0)
+  return x
+
+bacteria_feats = generate_data(n_samples = 20, n_feats = 2)
 
 def verif_new_data(t,h,p):
     n_clusters = centroids.shape[0]
@@ -42,6 +68,9 @@ def assign_clusters(data,centroids):
     assign = {}
     #new array for keeping the data of each data point in the cluster
     clusters = {}
+    
+    for i in range(n_clusters):
+        clusters[i] = []
     
     # iterating through the data array and searching the corresponding cluster for each data point
     for i in range(n_samples):
@@ -86,7 +115,7 @@ def kmeans(data, k):
         #the ending loop condition(if the centroids don't change anymore)
         if old_assign == assign:
             break
-    return centroids, assignment
+    return centroids, assign
 
 #calling the function for the needed variables
 np.random.seed(121)
